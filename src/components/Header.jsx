eb02d9ef7742a06dbaccddd60f82aec70da82ca5
flaviare1s <flaviarster@gmail.com>
2025-09-25 20:02:58 -0300

    feat: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,19 @@
+import logo from "../assets/img/miaulogo.png"
+import { MenuDesktop } from "./MenuDesktop"
+import { MenuMobile } from "./MenuMobile"
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header className="mb-2 flex justify-between items-center p-4 bg-light">
+      <div className="w-[120px]">
+        <img className="w-full" src={logo} alt="Logo da MiAuDota" />
+      </div>
+      <div className="hidden sm:block">
+        <MenuDesktop />
+      </div>
+      <div className="sm:hidden">
+        <MenuMobile />
+      </div>
+    </header> 
   )
 }
