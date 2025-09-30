307d1da20ba2a28daa8acc09d6320271e751f177
daniel cooder <danielcooder@gmail.com>
2025-09-30 11:33:16 -0300

    feat/header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import logo from "../assets/img/miaulogo.png"
 import { MenuDesktop } from "./MenuDesktop"
 import { MenuMobile } from "./MenuMobile"
+import { Link } from "react-router-dom"
 
 export const Header = () => {
   return (
     <header className="mb-2 flex justify-between items-center p-4 bg-light">
-      <div className="w-[120px]">
-        <img className="w-full" src={logo} alt="Logo da MiAuDota" />
+      <div className="w-[80px] sm:w-[120px]">
+        <Link to="/">
+          <img className="w-full" src={logo} alt="Logo da MiAuDota" />
+        </Link>
       </div>
       <div className="hidden sm:block">
         <MenuDesktop />
@@ -14,6 +17,6 @@ export const Header = () => {
       <div className="sm:hidden">
         <MenuMobile />
       </div>
-    </header> 
+    </header>
   )
 }
