70c219589933aa7e6340a193c1862a784fc7519c
flaviare1s <flaviarster@gmail.com>
2025-09-30 15:45:42 -0300

    feat: InputField

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,5 +1,30 @@
-export const InputField = () => {
+export const InputField = ({
+  label,
+  type,
+  id,
+  name,
+  placeholder,
+  value,
+  error,
+  register,
+  required = false,
+  validation = {},
+}) => {
+  const validations = { ...validation };
+  if (required) validations.required = typeof required === "string" ? required : "Campo obrigatório";
+
   return (
-    <div>InputField</div>
-  )
-}
+    <div className="flex flex-col gap-1 mb-4 p-4">
+      {label && <label htmlFor={id}>{label}</label>}
+      <input
+        className="border border-gray-300 rounded-md p-2"
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        {...(register && name ? register(name, validations) : {})}
+      />
+      {error && <small className="text-red-500">{error}</small>}
+    </div>
+  );
+};
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,24 @@
+import { useForm } from "react-hook-form"
+import { InputField } from "../components/InputField"
+
 export const Register = () => {
+  const { register, formState: { errors }, handleSubmit } = useForm();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
-    <div>Register</div>
+    <div className="min-h-screen flex flex-col justify-center items-center gap-4">
+      <div>
+        <h1 className="text-center">Cadastro</h1>
+        <form className="px-4 sm:min-w-[500px] m-auto flex flex-col justify-center items-center shadow" onSubmit={handleSubmit(onSubmit)}>
+          <InputField label="Nome:" placeholder="Digite seu nome" type="text" id="nomeCompleto" name="nomeCompleto" register={register} error={errors.nome?.message} required />
+          <InputField label="E-mail:" placeholder="Digite seu e-mail" type="email" id="email" name="email" register={register} error={errors.email?.message} required />
+          <InputField type="hidden" value="USUARIO" register={register} name="role" />
+          <button type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
   )
 }
