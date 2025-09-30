8f4c2edec3f66d82f27f7e70af322dbc2682bdd9
flaviare1s <flaviarster@gmail.com>
2025-09-30 13:51:31 -0300

    feat: menu mobile

--- src/components/MenuDesktop.jsx
+++ src/components/MenuDesktop.jsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom"
 export const MenuDesktop = () => {
   return (
     <nav className="flex gap-4 items-center">
-      <Link to="/">Home</Link>
-      <Link to="/about">Sobre</Link>
-      <Link className="bg-laranja py-2 px-3 text-light font-bold uppercase rounded-lg hover:bg-azul" to="/login">Login</Link>
+      <Link className="link-nav font-semibold" to="/">Home</Link>
+      <Link className="link-nav font-semibold" to="/about">Sobre</Link>
+      <Link className="link-nav font-semibold" to="/pets">Pets</Link>
+      <Link className="link-nav font-semibold" to="/register">Cadastro</Link>
     </nav>
   )
 }
