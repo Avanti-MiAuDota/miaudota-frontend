cdbfb92495980cce0065679d530df30dacc7ed4b
flaviare1s <flaviarster@gmail.com>
2025-10-08 06:32:00 -0300

    feat: add login screen (styles)

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -7,7 +7,7 @@ export const SubmitButton = ({ text }) => {
       }}
       className="w-full text-white font-semibold py-2 px-4 rounded-md 
                  hover:opacity-90 active:opacity-80 focus:outline-none 
-                 focus:ring-2 focus:ring-blue-300 transition-all duration-200"
+                 focus:ring-2 focus:ring-blue-300 transition-all duration-200 cursor-pointer"
     >
       {text}
     </button>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,37 @@
-export const Login = () => {
+import { InputField } from "../components/InputField";
+import { SubmitButton } from "../components/SubmitButton";
+
+export function Login() {
+
+  const handleSubmit = async () => {
+    console.log("submit")
+
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="flex justify-center items-center min-h-screen bg-gray-100 px-4">
+      <form
+        noValidate
+        onSubmit={handleSubmit}
+        className="bg-white p-6 sm:p-8 rounded-2xl shadow-md w-full max-w-md"
+      >
+        <h1 className="text-2xl font-bold mb-6 text-center text-gray-800">
+          Login
+        </h1>
+
+        <InputField
+          label="E-mail"
+          type="email"
+          name="email"
+        />
+
+        <InputField
+          label="Senha"
+          type="password"
+          name="senha"
+        />
+        <SubmitButton text="Login" />
+      </form>
+    </div>
+  );
 }
