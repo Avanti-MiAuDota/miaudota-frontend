677d2aeba00a534b943b1ec1ee5e880524bd117d
flaviare1s <flaviarster@gmail.com>
2025-10-08 08:32:07 -0300

    feat: private routes

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import { Adoption } from "./pages/Adoption"
 import { useAuth } from "./contexts/AuthContext"
 import { useEffect, useState } from "react"
 import { CustomLoader } from "./components/CustomLoader"
+import { PrivateRoute } from "./components/PrivateRoute"
 
 function App() {
   const { user } = useAuth();
@@ -43,13 +44,13 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/pets" element={<Pets />} />
           <Route path="/pets/:id" element={<PetProfile />} />
-          <Route path="/pets/add" element={<PetRegister />} />
-          <Route path="/pets/edit/:id" element={<PetEdit />} />
+          <Route path="/pets/add" element={<PrivateRoute><PetRegister /></PrivateRoute>} />
+          <Route path="/pets/edit/:id" element={<PrivateRoute><PetEdit /></PrivateRoute>} />
           <Route path="/pets/:petId/adoptions" element={<Adoptions />} />
           <Route path="/adoptions/:adoptionId" element={<Adoption />} />
           <Route path="/pets/adopt/:petId" element={<FormAdoption />} />
           <Route path="/congratulations" element={<Match />} />
-          <Route path="/401" element={<Unauthorized />} />
+          <Route path="/forbidden" element={<Unauthorized />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
