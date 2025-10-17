5f19b39b172bc43b3a7a7793787edb1d9d967003
daniel cooder <danielcooder@gmail.com>
2025-10-16 22:01:43 -0300

    Adiciona página UserProfile com rota /profile/:id e integração com API de usuário para exibir, atualizar e deletar dados

--- src/api/usuario.js
+++ src/api/usuario.js
@@ -4,3 +4,29 @@ export const createUsuario = async (usuarioData) => {
   const { data } = await api.post("/usuarios/", usuarioData);
   return data;
 };
+
+export const getUsuarioById = async (id) => {
+  const { data } = await api.get(`/usuarios/${id}`);
+  console.info('data retornada ', data);
+  return data;
+};
+
+export const updateUsuario = async (id, usuarioData) => {
+  const token = localStorage.getItem("token");
+  const { data } = await api.put(`/usuarios/${id}`, usuarioData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
+
+export const deleteUsuario = async (id) => {
+  const token = localStorage.getItem("token");
+  const { data } = await api.delete(`/usuarios/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
