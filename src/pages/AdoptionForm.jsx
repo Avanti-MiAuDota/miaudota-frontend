d7841c3e7ca65a5a2eb7fcdc787f7352a2296abc
flaviare1s <flaviarster@gmail.com>
2025-10-19 06:39:51 -0300

    feat: adocao.js

--- src/pages/AdoptionForm.jsx
+++ src/pages/AdoptionForm.jsx
@@ -1,12 +1,12 @@
 import { ReturnButton } from "../components/ReturnButton"
 
-export const FormAdoption = () => {
+export const AdoptionForm = () => {
   return (
     <div className="relative min-h-[calc(100vh-100px)] bg-gray-100 px-4">
     <div className ="absolute top-6 left-6"> 
       <ReturnButton />
       </div>
-    <div className="pt-17">FormAdoption</div>
+    <div className="pt-17">AdoptionForm</div>
     </div>
   )
 }
